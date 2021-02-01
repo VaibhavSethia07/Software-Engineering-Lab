4324f846b250b5829d8372ac983b5871d13d0e78
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2021-02-01 11:28:56 +0530

    Add quick sort program for Lab 3

---
+++ SE Lab 3/src/QuickSort.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class QuickSort {
+
+	private void quickSort(long nums[], int start, int end) {
+		if (start >= end)
+			return;
+		int p = partition(nums, start, end);
+		quickSort(nums, start, p-1);
+		quickSort(nums, p + 1, end);
+	}
+
+	private int partition(long nums[], int start, int end) {
+		int ind = start-1;
+
+		for (int i = start; i < end; i++) {
+			if (nums[i] <= nums[end]) {
+				ind++;
+				swap(nums, ind, start);
+			}
+		}
+		ind++;
+		swap(nums, ind, end);
+
+		return ind;
+	}
+
+	private void swap(long nums[], int i, int j) {
+		long temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+	private void printNums(long nums[], int N) throws Exception {
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		for (int i = 0; i < N; i++) {
+			out.write(nums[i] + " ");
+		}
+		out.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		long nums[] = Arrays.stream(br.readLine().split("\\s+")).mapToLong(Long::parseLong).toArray();
+
+		QuickSort qs = new QuickSort();
+		qs.quickSort(nums, 0, N - 1);
+		qs.printNums(nums, N);
+
+	}
+
+}
